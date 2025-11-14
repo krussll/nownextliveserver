641981483f0995841d4274f40a940953b3d4503e
krussll <raw987@hotmail.com>
2025-11-14 10:06:17 +0000

    Add files via upload

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
+app.use(cors());
 const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
